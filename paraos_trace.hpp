d7c3bb75efd9f595d9d3c7699aaf866ce0a65dc2
Mickle Isaev <mrraptor26@gmail.com>
2024-07-22 10:39:08 +0300

    feat: Added trace messages macros

---
+++ paraos_trace.hpp
@@ -0,0 +1,11 @@
+#ifndef PARAOS_TRACE_HPP
+#define PARAOS_TRACE_HPP
+
+#ifdef paraosTRACE_ENABLE
+#include <iostream>
+#define paraosTRACE_MESSAGE(message) std::cout << (message) << std::endl
+#else
+#define paraosTRACE_MESSAGE(message)
+#endif
+
+#endif /* PARAOS_TRACE_HPP */
