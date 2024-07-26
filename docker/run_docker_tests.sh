3cbe52ea9d503f7eeb46be9077808072bd9a9fe7
VyhodcevEgor <vyhodcev@internet.ru>
2024-07-26 14:44:43 +0300

    Улучшена работа скрипта сборки Docker образа и запуска в нём тестов.
    - Добавлена остановка запуска образа в случае неудачной сборки.
    - Если во время сборки возникнет ошибка, вывод в консоли станет красным.
    - Добавлены флаги для процесса тестирования с проверкой на утечки памяти.

--- docker/run_docker_tests.sh
+++ docker/run_docker_tests.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+RED=$(tput setaf 1)
+NC=$(tput sgr0)
+
+build_script="sh ./dbuild.sh"
+
 echo "Запуск сборки docker."
-sh ./dbuild.sh
-echo "Запуск docker conatiner."
-sh ./drunner.sh
+
+if eval "$build_script" == 0; then
+    echo $?
+    echo "Запуск docker conatiner."
+    sh ./drunner.sh
+else
+    echo "${RED}ERROR: Произошла ошибка во время сборки Docker образа!"
+fi
 
 read  -p  "Нажмите клавишу [Enter] чтобы завершить выполнение скрипта"
