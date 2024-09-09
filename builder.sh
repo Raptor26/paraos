abd186fead3c5b355a5a896a5e315223a271fd82
Mickle Isaev <mrraptor26@gmail.com>
2024-09-09 12:16:13 +0300

    feat: Added build bash script

---
+++ builder.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+GREEN=$(tput setaf 2)
+RED=$(tput setaf 1)
+NC=$(tput sgr0)
+
+
+pc_debug_clang="cmake --preset pc_debug_clang \
+    && cmake --build build/pc_debug_clang/ \
+    && ctest --test-dir build/pc_debug_clang \
+        -j8 \
+        --timeout 15 \
+        --repeat-until-fail 2 \
+        --schedule-random \
+        --output-on-failure"
+
+
+echo "Выберите необходимое действие"
+
+echo "  0 - Выход"
+echo "  1 - Запустить все сборки и тесты"
+echo "  2 - pc_debug_clang"
+echo "  12 - run tests in docker"
+
+read doing # Чтение переменной из стандартного ввода
+
+
+case $doing in
+0)
+    exit 0;;
+1)
+    if eval "$pc_debug_clang"; then
+        echo "${GREEN}SUCCESS: Все тесты выполнены успешно${NC}"
+    else
+        echo "${RED}ERROR${NC}: Обнаружена ошибка в сборке или выполнении тестов"
+    fi;;
+2)
+    eval $pc_debug_clang;;
+12)
+    cd docker
+    ./run_docker_tests.sh;;
+
+*) # Если с клавиатуры введено действие, не описанное конструкцией case, то будет выведено сообщение ниже
+    echo "Указанное действие не поддерживается"
+
+esac # Конец оператора case.
+# ------------------------------------------------------------------------------
+
+read  -p  "Нажмите клавишу [Enter] чтобы завершить выполнение скрипта"
