e1de0a13f34425e3bd25429b113247bf86850bb9
Mickle Isaev <mrraptor26@gmail.com>
2024-09-11 09:56:15 +0300

    feat: Added runtime profiler

---
+++ paraos_runtime_profiler.hpp
@@ -0,0 +1,216 @@
+/// @file runtime_profiler.hpp
+/// @author Mickle Isaev (mrraptor26@gmail.com)
+///
+/// @copyright Copyright (c) 2024 Stilsoft
+///
+/// MIT License:
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the 'Software'), to
+/// deal in the Software without restriction, including without limitation the
+/// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+/// sell copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+/// IN THE SOFTWARE.
+
+#ifndef PARAOS_RUNTIME_PROFILER_HPP
+#define PARAOS_RUNTIME_PROFILER_HPP
+
+#if defined(_WIN32) || defined(_WIN64) || defined(__linux__) || \
+    defined(__unix__)
+#include <chrono>
+using std::chrono::duration_cast;
+using std::chrono::high_resolution_clock;
+using time_resolution = std::chrono::microseconds;
+using namespace std::chrono_literals;
+#endif
+
+#include <cstddef>
+#include <cstdint>
+
+#include "paraos_config.hpp"
+
+namespace paraos {
+
+/// Empty profiler -------------------------------------------------------------
+
+/// @brief "Пустой" профилировщик. Используется в качестве профилировщика "по
+/// умолчанию".
+struct EmptyProfiler final {
+  PARAOS_INLINE_TRIVIAL void Start() {}
+  PARAOS_INLINE_TRIVIAL std::size_t Stop() { return 0u; }
+  PARAOS_INLINE_TRIVIAL std::size_t LastDuration() { return 0u; }
+};
+
+/// Operation system high resolution timer profiler ----------------------------
+
+#if defined(_WIN32) || defined(_WIN64) || defined(__linux__) || \
+    defined(__unix__)
+/// @brief Профилировщик, предназначенный для использования в операционных
+/// системах общего назначения.
+struct OsProfiler final {
+  PARAOS_INLINE_OPERATIONS void Start() {
+    start_ = high_resolution_clock::now();
+  }
+
+  std::size_t Stop() {
+    end_ = high_resolution_clock::now();
+    duration_ = duration_cast<time_resolution>(end_ - start_).count();
+    return LastDuration();
+  }
+  [[nodiscard]] std::size_t LastDuration() { return duration_; }
+
+ private:
+  decltype(high_resolution_clock::now()) start_;
+  decltype(high_resolution_clock::now()) end_;
+  decltype(duration_cast<time_resolution>(end_ - start_).count()) duration_{0u};
+};
+#endif  // #if defined(_WIN32) || defined(_WIN64) || defined(__linux__) ||
+        // defined(__unix__)
+
+/// Embedded Profiler ----------------------------------------------------------
+
+struct HightCntDefault {};
+
+/// @brief Шаблон 32-х битного счетчика.
+/// @warning Пользовательский код не использует данную структуру, она
+/// применяется в 'EmbeddedProfiler'.
+/// @tparam LOW_ADDR Структура, используемая для получения указателя на младшие
+/// 16 бит счетчика.
+/// @tparam HIGHT_ADDR Структура, используемая для получения указателя на
+/// старшие 16 бит счетчика.
+template <typename LOW_ADDR, typename HIGHT_ADDR>
+struct EmbeddedProfilerCnt {
+  using cnt_t = std::uint32_t;
+  EmbeddedProfilerCnt() = default;
+
+  /// @brief Деструктор "по умолчанию".
+  /// @note Деструктор явно не объявлен виртуальным т.к. "EmbeddedProfilerCnt" и
+  /// "EmbeddedProfiler" не выделяют динамических ресурсов.
+  ~EmbeddedProfilerCnt() = default;
+
+  EmbeddedProfilerCnt(const EmbeddedProfilerCnt &other) = default;
+  EmbeddedProfilerCnt &operator=(const EmbeddedProfilerCnt &other) = default;
+
+  EmbeddedProfilerCnt(EmbeddedProfilerCnt &&other) = default;
+  EmbeddedProfilerCnt &operator=(EmbeddedProfilerCnt &&other) = default;
+
+  /// @brief Возвращает значение 32-х битного аппаратного счетчика на момент
+  /// вызова.
+  /// @return Возвращает переменную типа 'cnt_t' содержащую значение аппаратного
+  /// счетчика на момент вызова.
+  PARAOS_INLINE_TRIVIAL cnt_t GiveCnt() {
+    constexpr cnt_t hight_mask = 0xFFFF0000;
+    constexpr cnt_t low_mask = 0x0000FFFF;
+    constexpr cnt_t bites_shift = 16u;
+    return (
+        (((static_cast<cnt_t>(*hight)) << bites_shift) & hight_mask) |
+        ((static_cast<cnt_t>(*low)) & low_mask));
+  }
+
+ protected:
+  PARAOS_INLINE_TRIVIAL constexpr cnt_t GiveCntOverflowValue() {
+    return std::numeric_limits<std::uint32_t>::max();
+  }
+
+ private:
+  volatile std::uint16_t *hight = HIGHT_ADDR{}();
+  volatile std::uint16_t *low = LOW_ADDR{}();
+};
+
+/// @brief Специализация шаблона счетчика в случае использования 16-ти битного
+/// счетчика (вместо 32-х битного).
+/// @warning Пользовательский код не использует данную структуру, она
+/// применяется в 'EmbeddedProfiler'.
+/// @tparam LOW_ADDR Структура, используемая для получения указателя на младшие
+/// 16 бит счетчика.
+template <typename LOW_ADDR>
+struct EmbeddedProfilerCnt<LOW_ADDR, HightCntDefault> {
+  using cnt_t = std::uint32_t;
+
+  EmbeddedProfilerCnt() = default;
+
+  /// @brief Деструктор "по умолчанию".
+  /// @note Деструктор явно не объявлен виртуальным т.к. "EmbeddedProfilerCnt" и
+  /// "EmbeddedProfiler" не выделяют динамических ресурсов.
+  ~EmbeddedProfilerCnt() = default;
+
+  EmbeddedProfilerCnt(const EmbeddedProfilerCnt &other) = default;
+  EmbeddedProfilerCnt &operator=(const EmbeddedProfilerCnt &other) = default;
+
+  EmbeddedProfilerCnt(EmbeddedProfilerCnt &&other) = default;
+  EmbeddedProfilerCnt &operator=(EmbeddedProfilerCnt &&other) = default;
+
+  /// @brief Возвращает значение 316-х битного аппаратного счетчика на момент
+  /// вызова.
+  /// @return Возвращает переменную типа 'cnt_t' содержащую значение аппаратного
+  /// счетчика на момент вызова.
+  PARAOS_INLINE_TRIVIAL cnt_t GiveCnt() { return static_cast<cnt_t>(*low); }
+
+ protected:
+  PARAOS_INLINE_TRIVIAL constexpr cnt_t GiveCntOverflowValue() {
+    return std::numeric_limits<std::uint16_t>::max();
+  }
+
+ private:
+  volatile std::uint16_t *low = LOW_ADDR{}();
+};
+
+/// @brief Профилировщик, предназначенный для использования во встраиваемых
+/// системах.
+/// @tparam LOW_ADDR Структура, используемая для получения указателя на младшие
+/// 16 бит счетчика.
+/// @tparam HIGHT_ADDR Структура, используемая для получения указателя на
+/// старшие 16 бит счетчика.
+template <typename LOW_ADDR, typename HIGHT_ADDR = HightCntDefault>
+struct EmbeddedProfiler final
+    : public EmbeddedProfilerCnt<LOW_ADDR, HIGHT_ADDR> {
+  using cnt_t = std::uint32_t;
+
+  PARAOS_INLINE_OPERATIONS void Start() {
+    start_ = EmbeddedProfilerCnt<LOW_ADDR, HIGHT_ADDR>::GiveCnt();
+
+    // Необходимо сбросить счетчик переполнений чтобы при повторном вызове
+    // Stop() не учитывать уже учтенное переполнение.
+    overflow_cnt_ = 0u;
+  }
+
+  cnt_t Stop() {
+    cnt_t stop = EmbeddedProfilerCnt<LOW_ADDR, HIGHT_ADDR>::GiveCnt();
+
+    if (start_ > stop) {
+      ++overflow_cnt_;
+    }
+
+    // Вычисление периода между вызовами Start() и Stop() с учетом переполнения.
+    const auto &overflow_value =
+        EmbeddedProfilerCnt<LOW_ADDR, HIGHT_ADDR>::GiveCntOverflowValue();
+    duration_ = static_cast<cnt_t>(
+        (overflow_value * overflow_cnt_) + (stop - start_) + overflow_cnt_);
+
+    return LastDuration();
+  }
+
+  PARAOS_INLINE_TRIVIAL cnt_t LastDuration() {
+    return static_cast<cnt_t>(duration_);
+  }
+
+ private:
+  cnt_t start_{0u};
+  cnt_t duration_{0u};
+  cnt_t overflow_cnt_{0u};
+};
+
+}  // namespace paraos
+
+#endif /* PARAOS_RUNTIME_PROFILER_HPP */
