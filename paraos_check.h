c85f2592c7cd3b3e52ebe93f52ea55b8cf517cb8
Mickle Isaev <mrraptor26@gmail.com>
2024-09-16 15:18:25 +0300

    ref: Refactor loop macros definition

--- paraos_check.h
+++ paraos_check.h
@@ -1,54 +1,49 @@
-#ifndef IRTOS_CHECK_H
-#define IRTOS_CHECK_H
+#ifndef PARAOS_CHECK_H
+#define PARAOS_CHECK_H
 
 #include <assert.h>
 
 /* Define this macro to include build configuration header. This is an
  * alternative to the -D compiler flag.
  * Usage example with CMake:
- * "-DICORE_CHECK_CONFIG_HEADER="${CMAKE_CURRENT_SOURCE_DIR}/icore_check_config.h"
+ * "-DPARAOS_CHECK_CONFIG_HEADER="${CMAKE_CURRENT_SOURCE_DIR}/paraos_check_config.hpp"
  * -----------------------------------------------------------------------------
- * or
+ * or (example)
  * -----------------------------------------------------------------------------
  * target_compile_definitions(
- *   interfaces_core INTERFACE
- *   -DICORE_CHECK_CONFIG_HEADER="${CMAKE_CURRENT_SOURCE_DIR}/icore_check_config.h")
+ *   paraos_setup INTERFACE
+ *   -DPARAOS_CHECK_CONFIG_HEADER="${CMAKE_CURRENT_SOURCE_DIR}/paraos_check_config.hpp")
  */
 #ifdef PARAOS_CHECK_CONFIG_HEADER
 #include PARAOS_CHECK_CONFIG_HEADER
 #endif
 
 #ifdef PARAOS_CHECK_LOOP_ENABLE
-/* Если пользователь предоставил реализацию макроса LOOP */
-#ifdef PARAOS_CHECK_LOOP_USER_IMP
-#define PARAOS_CHECK_LOOP() PARAOS_CHECK_LOOP_USER_IMP()
+
+// If build system is windows or linux, we can provide definitions for
+// PARAOS_CHECK_LOOP() and PARAOS_CHECK_ASSERT()
+#if defined(__WIN32__) || defined(__WIN64__) || defined(__linux__) || \
+    defined(__unix__)
+
+#define PARAOS_CHECK_LOOP() assert(1 != 1)
+#define PARAOS_CHECK_ASSERT(x) assert(x)
+
+#elif !defined(PARAOS_CHECK_LOOP)
+#error \
+    "User code must provide PARAOS_CHECK_LOOP() macros for host platform which loop forever"
+#else
+// In other case, user code must provide PARAOS_CHECK_LOOP() definition and
+// after that we can provide PARAOS_CHECK_ASSERT() definition
 #define PARAOS_CHECK_ASSERT(x) \
   if ((x) == 0) {              \
     PARAOS_CHECK_LOOP();       \
   }
-#elif defined(__WIN32__) || defined(__WIN64__) || defined(__linux__) || \
-    defined(__unix__)
-/* Вызов макроса PARAOS_CHECK_LOOP() выполняет вызов assert() с ложным
- * условием */
-#define PARAOS_CHECK_LOOP() assert(1 != 1)
-#define PARAOS_CHECK_ASSERT(x) assert(x)
-#else
-/* В противном случае используется пустой макрос */
-#define PARAOS_CHECK_LOOP()
-#define PARAOS_CHECK_ASSERT(x)
 #endif
 #else
-/**
- * @brief В соответствие с конфигурацией <PARAOS_CHECK_LOOP_ENABLE> и
- * <PARAOS_CHECK_LOOP_USER_IMP>, данный макрос является пустым
- */
+
 #define PARAOS_CHECK_LOOP()
-
-/**
- * @brief В соответствие с конфигурацией <PARAOS_CHECK_LOOP_ENABLE> и
- * <PARAOS_CHECK_LOOP_USER_IMP>, данный макрос является пустым
- */
 #define PARAOS_CHECK_ASSERT(x)
-#endif
 
-#endif /* IRTOS_CHECK_H */
+#endif /* #ifdef PARAOS_CHECK_LOOP_ENABLE */
+
+#endif /* PARAOS_CHECK_H */
