49e8c74183277faeb53403279771d24e9bd04a95
Mickle Isaev <mrraptor26@gmail.com>
2024-09-18 15:41:35 +0300

    ref: Moved etl config in separate folder

--- port_unix/config/etl_profile.h
+++ port_unix/config/etl_profile.h
@@ -1,14 +1,7 @@
 #ifndef ETL_PROFILE_H
 #define ETL_PROFILE_H
 
-#if defined(_MSC_VER)
-#include "etl/profiles/msvc_x86.h"
-#elif defined(__GNUC__)
 #include "etl/profiles/gcc_generic.h"
-#endif
-
-#define ETL_DEVELOPMENT_OS_WINDOWS
-#define ETL_TARGET_OS_FREERTOS
 
 #define ETL_THROW_EXCEPTIONS
 #define ETL_VERBOSE_ERRORS
