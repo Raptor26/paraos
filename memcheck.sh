83776e9fbbbcb8f0e4db1ffbb105a01b24d20401
Isaev Mickle <mrraptor26@gmail.com>
2024-09-27 10:42:48 +0300

    fix: Fixed memory leak in FreeRTOS semaphore

--- memcheck.sh
+++ memcheck.sh
@@ -6,11 +6,11 @@
 
 echo "Memcheck unix"
 cmake --preset pc_debug_clang_docker \
-&& cmake --build build/pc_debug_clang_docker/ \
-&& ctest -T memcheck --test-dir build/pc_debug_clang_docker -j16 --timeout 15 --output-on-failure
+    && cmake --build build/pc_debug_clang_docker/ \
+    && ctest -T memcheck --test-dir build/pc_debug_clang_docker -j16 --timeout 15 --output-on-failure
 
 echo "Memcheck freeRTOS"
 cmake --preset freertos_debug_clang \
-&& cmake --build build/freertos_debug_clang/ \
-&& ctest -T memcheck --test-dir build/freertos_debug_clang -j16 --timeout 15 --output-on-failure -V
+    && cmake --build build/freertos_debug_clang/ \
+    && ctest -T memcheck --test-dir build/freertos_debug_clang -j16 --timeout 15 --output-on-failure
 
