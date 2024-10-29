a05eed85945d23a31889cbe5e5ab15e8abae9874
Mickle Isaev <mrraptor26@gmail.com>
2024-10-29 14:42:13 +0300

    ref: Added critical section for get and increment operations with global variable

--- containers/tests/test_message_multithread_many_producer_many_consumers.cpp
+++ containers/tests/test_message_multithread_many_producer_many_consumers.cpp
@@ -110,8 +110,12 @@ struct Producer : public paraos::Thread {
 
   /// @brief Producer thread.
   void Run() override {
-    std::size_t str_idx = producer_actual_str_idx;
-    ++producer_actual_str_idx;
+    std::size_t str_idx;
+    {
+      const paraos::CriticalSection critical;
+      str_idx = producer_actual_str_idx;
+      ++producer_actual_str_idx;
+    }
 
     if (str_idx < elems_vector.size()) {
       // Trying to write message in buffer will not work yet.
