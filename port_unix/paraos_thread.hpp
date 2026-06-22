024f03563ee531b0ab97dc89e6dcaec859b3f3db
Mickle Isaev <mrraptor26@gmail.com>
2026-06-22 11:39:29 +0300

    refactor(license): заменен текст лицензии на SPDX MIT во всех библиотечных файлах paraos
    - Заменен развернутый MIT-блок на краткую форму SPDX-License-Identifier.
    - Добавлена лицензия к файлам, где она отсутствовала.
    - Сохранены существующие авторы с нормализацией повторяющихся вариантов.
    - Исправлены устаревшие значения @file на реальные имена файлов.
    - Сторонние библиотеки и заимствованные конфиги оставлены без изменений.

--- port_unix/paraos_thread.hpp
+++ port_unix/paraos_thread.hpp
@@ -1,44 +1,8 @@
-/// @file paraos_thread_v2.hpp
+/// @file paraos_thread.hpp
 /// @author Mickle Isaev (mrraptor26@gmail.com)
 ///
-/// @copyright (c) 2025 Stilsoft
-///
-/// MIT License:
-///
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the 'Software'), to
-/// deal in the Software without restriction, including without limitation the
-/// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-/// sell copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-///
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-///
-/// THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-/// IN THE SOFTWARE.
-///
-/// NAME
-///     paraos_thread_v2.
-///
-/// DESCRIPTION
-///     paraos_thread_v2 provides a POSIX wrapper for working with threads.
-///     This wrapper offers a cross-platform API for managing threads
-///     in RTOS, Windows, and Linux.
-///
-/// EXAMPLE
-///     See usages example in:
-///     - port_tests/test_paraos_thread_only_global.cpp
-///     -
-///       port_tests/test_paraos_thread_only_stack_with_multiple_threads_in_one_object.cpp
-///     - port_tests/test_paraos_thread_only_stack.cpp
-///     - port_tests/test_paraos_thread_only_static.cpp
-
+/// SPDX-License-Identifier: MIT.
+/// See LICENSE file in the project root for full license information.
 #ifndef PARAOS_THREAD_V2_HPP
 #define PARAOS_THREAD_V2_HPP
 
--- port_win/config/etl_profile.h
+++ port_win/config/etl_profile.h
@@ -1,3 +1,8 @@
+/// @file etl_profile.h
+/// @author Mickle Isaev (mrraptor26@gmail.com)
+///
+/// SPDX-License-Identifier: MIT.
+/// See LICENSE file in the project root for full license information.
 #ifndef ETL_PROFILE_H
 #define ETL_PROFILE_H
 
