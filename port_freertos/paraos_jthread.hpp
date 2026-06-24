6f44a784c7914c50a5ce66dee83ca05617226f9a
Mickle Isaev <mrraptor26@gmail.com>
2026-06-24 16:29:08 +0300

    Revert "fix(freertos/jthread): удалён принудительный std::exit(0) из end_scheduler()"
    This reverts commit 090d4bcb6697d3ff3144694dc31032bd9aa729d0.

--- port_freertos/paraos_jthread.hpp
+++ port_freertos/paraos_jthread.hpp
@@ -7,6 +7,7 @@
 #define PARAOS_JTHREAD_HPP
 
 #include <cstddef>
+#include <cstdlib>
 #include <functional>
 #include <memory>
 #include <tuple>
@@ -216,7 +217,15 @@ class jthread {
     if (xTaskGetSchedulerState() == taskSCHEDULER_NOT_STARTED) {
       return false;
     }
+#define PARAOS_FORCE_EXIT_FROM_SCHEDULER
+#ifdef PARAOS_FORCE_EXIT_FROM_SCHEDULER
+    // В UNIX/Windows vTaskEndScheduler() не завершает корректно потоки, поэтому
+    // нужно принудительно выйти из программы.
+    std::exit(0);
+#else
     vTaskEndScheduler();
+#endif
+#undef PARAOS_FORCE_EXIT_FROM_SCHEDULER
     return true;
   }
 
